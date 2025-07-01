c1c586c1924281ee1996741ee0e8ab2c61db3bde
Lucas de Tarso <tarsoengenhariacivil@gmail.com>
2025-07-01 16:05:40 -0300

    Corrige erro de tela branca: importa AppRoutes corretamente

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./AppRoutes"; // <- ESSENCIAL
+import AppRoutes from "./AppRoutes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
