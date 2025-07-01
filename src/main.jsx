50d4fb391095d29dcca0df9f94d44603f520edd9
Lucas de Tarso <tarsoengenhariacivil@gmail.com>
2025-07-01 15:48:06 -0300

    Corrige importação do AppRoutes no main.jsx2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import AppRoutes from "./AppRoutes";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./AppRoutes";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
