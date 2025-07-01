b2c5811360fad72700c13de9ec303e13f449ba62
Lucas de Tarso <tarsoengenhariacivil@gmail.com>
2025-07-01 16:14:29 -0300

    Corrige definição do componente AppRoutes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -7,6 +7,15 @@ import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectItem } from "@/components/ui/select";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
-
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        {/* Suas rotas aqui, exemplo: */}
+        {/* <Route path="/" element={<Login />} /> */}
+      </Routes>
+    </Router>
+  );
+}
 
 export default AppRoutes;
